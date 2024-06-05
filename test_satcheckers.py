5d7f139db7a270b73c302c948ad2b740aabc4fdf
strago811 <strago811@icloud.com>
2024-06-05 16:44:26 -0400

    Update test_satcheckers.py

--- test_satcheckers.py
+++ test_satcheckers.py
@@ -16,9 +16,9 @@ from selenium.webdriver.support.ui import Select
 
 import smtplib
 
-email = "youremail@gmail.com"
-location = "00000" #your zip code
-distance = "10 miles" #whatever your want (has to be on the website as an option)
+email = "strago811@icloud.com"
+location = "07646" #your zip code
+distance = "100 miles" #whatever your want (has to be on the website as an option)
 class TestSatchecker():
     def setup_method(self):
         service = Service('chromedriver.exe')
